8a9b563210227c6d5f77258482dfeeff57ad53d0
richeasy001 <tenebe.osikhena@gmail.com>
2024-09-15 12:34:09 +0100

    variable correction

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-income = int(input("Enter your monthly income: "))
-exp = int(input("Enter your total monthly expenses: "))
-Monthly_Savings = income - exp
-Savings = int((Monthly_Savings * 12) + (Monthly_Savings * 12 * 0.05))
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+Monthly_Savings = monthly_income - monthly_expenses
+Savings = (Monthly_Savings * 12) + (Monthly_Savings * 12 * 0.05)
 print (f"Your monthly savings are ${Monthly_Savings}.")
 print (f"Projected savings after one year, with interest, is: ${Savings}.")
